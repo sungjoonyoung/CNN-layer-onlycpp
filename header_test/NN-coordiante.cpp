c90548264fddab1edd535ec4ec1e0cfd3c972e20
Joonyoung Sung Desktop <sungso376@gmail.com>
2025-11-24 01:56:12 +0900

    test header cpp

---
+++ header_test/NN-coordiante.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include"..\equipment_header\sungso376_LA.hpp"
+// #include"/equipment/sungso376_LA"
+using namespace std;
+int main(void){
+    
+    cout<<"\n\n\nA\n\n\n";
+
+    vector<double> v1(3,2); //(2,2,2)
+    vector<vector<double>> v2={
+        {2,1,0,1},
+        {2,1,0,4},
+    };
+    
+    for(auto c:NN_coordinate(v1,v2))cout<<c<<"\n";
+
+    
+}
--- header_test/inner.cpp
+++ header_test/inner.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 #include<vector>
-#include"equipment_header\sungso376_LA.hpp"
+#include"..\equipment_header\sungso376_LA.hpp"
 // #include"/equipment/sungso376_LA"
 using namespace std;
 int main(void){
     vector<double> v1(3,2);
     vector<double> v2(3,3);
     cout<<"\n\n\nA\n\n\n";
-    cout<<inner_product(v1,v2);
+    cout<<inner_product(v1,v2)<<"\n";
+
+    
 }
